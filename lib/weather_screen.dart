c878986dcbda8bf842ca9b1e4ced14d5903c3c7a
msiyem <siyem2574@gmail.com>
2025-06-26 16:56:01 +0600

    weather app

---
+++ lib/weather_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Weather App',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              
+            },
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      
+    );
+  }
+}
